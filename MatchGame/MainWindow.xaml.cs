b15fd6ceb20d90f7de3ab1d5d644e3523ab7789d
JimmyTropicana <rickey9986@gmail.com>
2024-01-20 15:48:06 -0600

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            // create a list of eight pairs of emojis
+            List<string> lazarEmoji = new List<string>()
+            {
+                "🍄", "🍄",
+                "🥑", "🥑",
+                "🦜", "🦜",
+                "🦆", "🦆",
+                "👽", "👽",
+                "👻", "👻",
+                "🎷", "🎷",
+                "🎨","🎨",
+            };
+            
+            // create a new random number generator
+            Random random = new Random();
+
+            // find every TextBlock in the main grid and repeat the following for each one
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                // pick a random number between 0 and the number of emoji left in the list and call it "index"
+                int index = random.Next(lazarEmoji.Count);
+                // use the random number called "index" to get a random emoji from the list
+                string nextEmoji = lazarEmoji[index];
+                // update the TextBlock with the random emoji
+                textBlock.Text = nextEmoji;
+                // remove that emoji from the list
+                lazarEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
