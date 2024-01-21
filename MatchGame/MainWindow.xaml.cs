5b8de717169f4d285ee025bf198bd89ab9f92cf7
JimmyTropicana <rickey9986@gmail.com>
2024-01-20 19:55:03 -0600

    done but timer sucks

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,10 +21,28 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play Again?";
+            }
         }
 
         private void SetUpGame()
@@ -44,17 +63,58 @@ namespace MatchGame
             // create a new random number generator
             Random random = new Random();
 
-            // find every TextBlock in the main grid and repeat the following for each one
+            // find every TextBlock in the main grid and repeat the following statements for each one
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                // pick a random number between 0 and the number of emoji left in the list and call it "index"
-                int index = random.Next(lazarEmoji.Count);
-                // use the random number called "index" to get a random emoji from the list
-                string nextEmoji = lazarEmoji[index];
-                // update the TextBlock with the random emoji
-                textBlock.Text = nextEmoji;
-                // remove that emoji from the list
-                lazarEmoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    // pick a random number between 0 and the number of emoji left in the list and call it "index"
+                    int index = random.Next(lazarEmoji.Count);
+                    // use the random number called "index" to get a random emoji from the list
+                    string nextEmoji = lazarEmoji[index];
+                    // update the TextBlock with the random emoji
+                    textBlock.Text = nextEmoji;
+                    // remove that emoji from the list
+                    lazarEmoji.RemoveAt(index);
+                }
+            }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
